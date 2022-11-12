9d3c69c5e8d6c048bbb3b664c132f6f0b5e222d9
pankajagrawal16 <pnkaj.agrwal@gmail.com>
2022-11-12 15:02:18 +0100

    Disable help links

--- frontend/src/GlobalConstants.ts
+++ frontend/src/GlobalConstants.ts
@@ -16,11 +16,11 @@ GLOBAL_CONSTANTS.set('PYTHON', {
 });
 
 export const Links_List = [
-    {label: 'Source code for the project', link: 'https://github.com/aws-samples/serverless-webapp-mono-repo-ci-cd-java'},
-    {label: 'Architecture diagram', link: '/serverless-webapp-mono-repo-ci-cd-java.png'},
-    {label: 'Amazon Rekognition', link: 'https://aws.amazon.com/rekognition/'},
-    {label: 'AWS Serverless Application Model', link: 'https://aws.amazon.com/serverless/sam/'},
-    {label: 'AWS Cloud Development Kit (AWS CDK)', link: 'https://docs.aws.amazon.com/cdk/latest/guide/work-with-cdk-java.html'},
-    {label: 'Learn more about AWS developer tools services?', link: 'https://aws.amazon.com/products/developer-tools/'},
+    {label: 'Source code for the project', link: 'https://github.com/pankajagrawal16/serverless-webapp-mono-repo-ci-cd-java'},
+   // {label: 'Architecture diagram', link: '/serverless-webapp-mono-repo-ci-cd-java.png'},
+  //  {label: 'Amazon Rekognition', link: 'https://aws.amazon.com/rekognition/'},
+  //  {label: 'AWS Serverless Application Model', link: 'https://aws.amazon.com/serverless/sam/'},
+   // {label: 'AWS Cloud Development Kit (AWS CDK)', link: 'https://docs.aws.amazon.com/cdk/latest/guide/work-with-cdk-java.html'},
+   // {label: 'Learn more about AWS developer tools services?', link: 'https://aws.amazon.com/products/developer-tools/'},
     {label: 'Connect with me @agrawalpankaj16', link: 'https://twitter.com/agrawalpankaj16'},
 ];
